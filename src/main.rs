c37e365f5175c86bbb078d967b661f30ff3be6e0
sarendsen <168401+sarendsen@users.noreply.github.com>
2023-02-07 15:49:54 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 extern crate pretty_env_logger;
 use anyhow::Result;
-use http::{HeaderMap, HeaderValue};
 use cached::proc_macro::cached;
+use http::Method;
+use http::{HeaderMap, HeaderValue};
 
 // #[macro_use] extern crate log;
-use plex_api::{ HttpClientBuilder};
+use plex_api::HttpClientBuilder;
 use serde::{Deserialize, Serialize};
 // use serde_json::Result;
 use serde_xml_rs::from_str as from_xml_str;
 use serde_xml_rs::to_string as to_xml_str;
+// use quick_xml::de::from_str as from_xml_str;
+// use quick_xml::se::to_string as to_xml_str;
 // use serde_xml_rs::from_reader
 
 use hyper::server::conn::AddrStream;
@@ -16,7 +19,6 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server, StatusCode};
 use std::net::IpAddr;
 use std::{convert::Infallible, net::SocketAddr};
-
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "tests_deny_unknown_fields", serde(deny_unknown_fields))]
@@ -37,14 +39,22 @@ pub struct MetaData {
 #[cfg_attr(feature = "tests_deny_unknown_fields", serde(deny_unknown_fields))]
 #[serde(rename_all = "camelCase")]
 pub struct Hub {
+    // #[serde(rename = "@key")]
     key: String,
     hub_key: Option<String>,
+    // #[serde(rename = "@title")]
     title: String,
+    // #[serde(rename = "@hubIdentifier")]
     hub_identifier: String,
+    // #[serde(rename = "@context")]
     context: String,
+    // #[serde(rename = "@type")]
     r#type: String,
+    // #[serde(rename = "@size")]
     size: i32,
+    // #[serde(rename = "@more")]
     more: bool,
+    // #[serde(rename = "@style")]
     style: String,
     promoted: Option<bool>,
     #[serde(rename = "Metadata")]
@@ -55,8 +65,10 @@ pub struct Hub {
 #[cfg_attr(feature = "tests_deny_unknown_fields", serde(deny_unknown_fields))]
 #[serde(rename_all = "camelCase")]
 pub struct MediaContainer {
-    pub size: usize,
-    pub allow_sync: bool,
+    // #[serde(rename = "@size")]
+    pub size: Option<usize>,
+    // #[serde(rename = "@allowSync")]
+    pub allow_sync: Option<bool>,
     pub identifier: Option<String>,
     #[serde(rename = "librarySectionID")]
     pub library_section_id: Option<u32>,
@@ -65,6 +77,8 @@ pub struct MediaContainer {
     pub library_section_uuid: Option<String>,
     #[serde(rename = "Hub")]
     pub hub: Option<Vec<Hub>>,
+    // #[serde(default)]
+    // pub item: Option<Vec<Hub>>,
     #[serde(rename = "Metadata")]
     metadata: Option<Vec<MetaData>>,
 }
@@ -74,9 +88,18 @@ pub struct MediaContainer {
 #[serde(rename_all = "camelCase")]
 pub struct MediaContainerWrapper<T> {
     #[serde(rename = "MediaContainer")]
+    // #[serde(rename="$value")]
     pub media_container: T,
 }
 
+// #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+// #[cfg_attr(feature = "tests_deny_unknown_fields", serde(deny_unknown_fields))]
+// #[serde(rename_all = "camelCase")]
+// pub struct MediaContainerWrapperXML<MediaContainer> {
+// }
+
+// pub struct MediaContainerWrapper<MediaContainer> {
+// }
 
 // struct PlexHttpClient {
 //     pub api_url: String,
@@ -101,13 +124,13 @@ pub struct MediaContainerWrapper<T> {
 //     // }
 // }
 
-#[cached(time=360)]
-async fn get_custom_collections() -> Vec<MetaData> {
+#[cached(time = 360)]
+async fn get_custom_collections(token: String) -> Vec<MetaData> {
     // TODO: Use hyper Client or hyper_reverse_proxy
     let client = HttpClientBuilder::default()
         .set_api_url("https://plex.sjoerdarendsen.dev")
-        .set_x_plex_token("RrZN1WRwYYfao2cuiHs5".to_owned())
-        .set_x_plex_client_identifier("etz23lqlxhsdinb7hv9uiu38".to_owned())
+        .set_x_plex_token(token)
+        //.set_x_plex_client_identifier("etz23lqlxhsdinb7hv9uiu38".to_owned())
         .build()
         .expect("wut went wrong");
 
@@ -129,7 +152,6 @@ async fn get_custom_collections() -> Vec<MetaData> {
     .concat()
 }
 
-
 // fn get_content_type_from_response(resp: &Response<Body>) -> ContentType {
 fn get_content_type_from_headers(headers: &HeaderMap<HeaderValue>) -> ContentType {
     //let wut = resp.headers().get("content-type").unwrap().
@@ -147,6 +169,7 @@ fn get_content_type_from_headers(headers: &HeaderMap<HeaderValue>) -> ContentTyp
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum ContentType {
     Json,
     Xml,
@@ -155,6 +178,7 @@ enum ContentType {
 async fn body_to_string(body: Body) -> Result<String> {
     let body_bytes = hyper::body::to_bytes(body).await?;
     let string = String::from_utf8(body_bytes.to_vec())?;
+    // Ok(string.replace('\n', ""))
     Ok(string)
 }
 
@@ -163,11 +187,19 @@ async fn from_body(
     body: Body,
     content_type: &ContentType,
 ) -> Result<MediaContainerWrapper<MediaContainer>> {
+    // println!("original Response body: {:#?}", content_type);
     let body_string = body_to_string(body).await?;
-
+    // println!("original Response body: {:#?}", body_string);
     let result: MediaContainerWrapper<MediaContainer> = match content_type {
         ContentType::Json => serde_json::from_str(&body_string).unwrap(),
-        ContentType::Xml => from_xml_str(&body_string).unwrap(),
+        ContentType::Xml => {
+            //let fake = format!("<MediaContainerWrapper/>{}</MediaContainerWrapper>", body_string);
+            //println!("original Response body: {:#?}", fake);
+            let yup: MediaContainer = from_xml_str(&body_string).unwrap();
+            MediaContainerWrapper {
+                media_container: yup,
+            }
+        }
     };
     Ok(result)
 }
@@ -179,10 +211,9 @@ async fn to_string(
 ) -> Result<String> {
     match content_type {
         ContentType::Json => Ok(serde_json::to_string(&container).unwrap()),
-        ContentType::Xml => Ok(to_xml_str(&container).unwrap()),
+        ContentType::Xml => Ok(to_xml_str(&container.media_container).unwrap()),
     }
 }
-
 
 // fn create_proxied_response(mut resp: Response<Body>, body: Body) -> Response<Body> {
 //     // *response.headers_mut() = remove_hop_headers(response.headers());
@@ -194,20 +225,36 @@ async fn handle(client_ip: IpAddr, mut req: Request<Body>) -> Result<Response<Bo
     // Default is gzip. Dont want that
     req.headers_mut()
         .insert("Accept-Encoding", HeaderValue::from_static("identity"));
+    println!("Proxy req: {:#?}", req);
     let uri = req.uri_mut().to_owned();
-
+    let method = req.method_mut().to_owned();
+    let token = req
+        .headers()
+        .get("x-plex-token")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+    // let xml_string = std::fs::read_to_string("test/hubs.xml").unwrap();
+    // println!("{:#?}", xml_string);
     match hyper_reverse_proxy::call(client_ip, "http://100.91.35.113:32400", req).await {
         Ok(resp) => {
-            if uri.path().starts_with("/hubs/sections") {
+            if uri.path().starts_with("/hubs/sections") && method == Method::GET {
                 let (mut parts, body) = resp.into_parts();
                 let content_type = get_content_type_from_headers(&parts.headers);
                 let mut container = from_body(body, &content_type).await?;
-                container = patch_hubs(container).await;
+
+                container = patch_hubs(container, token).await;
                 let body_string = to_string(container, &content_type).await?;
                 let transformed_body = Body::from(body_string);
                 parts.headers.remove("content-length");
-                parts.headers.insert("x-plex-proxy", HeaderValue::from_static("true"));
+                parts
+                    .headers
+                    .insert("x-plex-proxy", HeaderValue::from_static("true"));
                 let transformed_response = Response::from_parts(parts, transformed_body);
+
+                println!("transformed Response: {:#?}", transformed_response);
+                println!("----------");
                 return Ok(transformed_response);
             }
             Ok(resp)
@@ -241,6 +288,7 @@ async fn main() {
 
 async fn patch_hubs(
     mut container: MediaContainerWrapper<MediaContainer>,
+    token: String,
 ) -> MediaContainerWrapper<MediaContainer> {
     if container.media_container.hub.is_none() {
         // nothing todo
@@ -250,7 +298,7 @@ async fn patch_hubs(
     let collections = container.media_container.hub.unwrap();
     // println!("{:#?}", hub_collections.len());
 
-    let custom_collections = get_custom_collections().await;
+    let custom_collections = get_custom_collections(token).await;
 
     let custom_collections_keys: Vec<String> =
         custom_collections.iter().map(|c| c.key.clone()).collect();
@@ -261,12 +309,12 @@ async fn patch_hubs(
             c.context != "hub.custom.collection" || custom_collections_keys.contains(&c.key)
         })
         .collect();
-    
+
     // println!("{:#?}", new_collections.len());
 
     let size = new_collections.len();
     container.media_container.hub = Some(new_collections);
-    container.media_container.size = size;
+    container.media_container.size = Some(size);
     container
 }
 
@@ -276,40 +324,49 @@ mod tests {
     #[cfg(test)]
     use pretty_assertions::assert_eq;
 
-    #[test]
-    fn generic_test() {
-        let json_string = std::fs::read_to_string("test/hubs.json").unwrap();
-        let result: MediaContainerWrapper<MediaContainer> =
-            serde_json::from_str(&json_string).unwrap();
-        // println!("{:?}", result);
-        println!("{:#?}", result);
-        let entry: MediaContainerWrapper<MediaContainer> = MediaContainerWrapper {
-            media_container: MediaContainer {
-                size: 11,
-                identifier: Some("com.plexapp.plugins.library".to_owned()),
-                library_section_id: 1,
-                hub: Some(vec![]),
-                metadata: Some(vec![]),
-            },
-        };
-        assert_eq!(entry, result);
-    }
+    // #[test]
+    // fn generic_test() {
+    //     let json_string = std::fs::read_to_string("test/hubs.json").unwrap();
+    //     let result: MediaContainerWrapper<MediaContainer> =
+    //         serde_json::from_str(&json_string).unwrap();
+    //     // println!("{:?}", result);
+    //     println!("{:#?}", result);
+    //     let entry: MediaContainerWrapper<MediaContainer> = MediaContainerWrapper {
+    //         media_container: MediaContainer {
+    //             size: Some(11),
+    //             identifier: Some("com.plexapp.plugins.library".to_owned()),
+    //             library_section_id: Some(1),
+    //             allow_sync: Some(false),
+    //             library_section_title: Some("emty".to_owned()),
+    //             library_section_uuid: Some("emty".to_owned()),
+    //             hub: Some(vec![]),
+    //             metadata: Some(vec![]),
+    //         },
+    //     };
+    //     assert_eq!(entry, result);
+    // }
 
     #[test]
     fn xml_test() {
         let xml_string = std::fs::read_to_string("test/hubs.xml").unwrap();
-        let result: MediaContainerWrapper<MediaContainer> = from_xml_str(&xml_string).unwrap();
+        // let result: MediaContainerWrapper<MediaContainer> = MediaContainerWrapper {
+        //     media_container: from_xml_str(&xml_string).unwrap(),
+        // };
+        let result: MediaContainer = from_xml_str(&xml_string).unwrap();
         // println!("{:?}", result);
         println!("{:#?}", result);
-        let entry: MediaContainerWrapper<MediaContainer> = MediaContainerWrapper {
-            media_container: MediaContainer {
-                size: 11,
-                identifier: Some("com.plexapp.plugins.library".to_owned()),
-                library_section_id: 1,
-                hub: Some(vec![]),
-                metadata: Some(vec![]),
-            },
-        };
-        assert_eq!(entry, result);
+        // let entry: MediaContainer = MediaContainerWrapper {
+        //     media_container: MediaContainer {
+        //         size: 11,
+        //         identifier: Some("com.plexapp.plugins.library".to_owned()),
+        //         library_section_id: Some(1),
+        //         allow_sync: false,
+        //         library_section_title: Some("emty".to_owned()),
+        //         library_section_uuid: Some("emty".to_owned()),
+        //         hub: Some(vec![]),
+        //         metadata: Some(vec![]),
+        //     },
+        // };
+        // assert_eq!(entry, result);
     }
 }
