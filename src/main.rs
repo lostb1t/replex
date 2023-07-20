8f70981db32a0679ee700b1d88e355e31872f725
sarendsen <168401+sarendsen@users.noreply.github.com>
2023-07-20 18:14:44 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ fn router(proxy: Proxy) -> Router {
             get(get_hubs_sections),
         )
         .route(
-            "/hubs/library/collections/:ids/children",
+            "/httplex/library/collections/:ids/children",
             get(get_collections_children),
         )
         // .route(
