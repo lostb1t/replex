6920cb11ea45a5781c8bffedd7337804aa4be7c6
sarendsen <168401+sarendsen@users.noreply.github.com>
2023-07-22 10:45:29 +0200

    Cargo fix

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# TODO: remove protocol for nginx upstream block
+http="http://"
+https="http://"
+NGINX_PLEX=$(echo $REPLEX_HOST)
+NGINX_PLEX=${REPLEX_HOST#"$http"}
+NGINX_PLEX=${REPLEX_HOST#"$https"}
+export NGINX_PLEX
+
 ./app/replex &
 
 ./docker-entrypoint.sh "nginx" &
