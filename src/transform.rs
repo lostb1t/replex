1b82b141fd806b37aadcd917e1798a66497f70ea
sarendsen <coding-mosses0z@icloud.com>
2023-08-11 15:29:06 +0200

    fix: disable replacing the thumbfor now

--- src/transform.rs
+++ src/transform.rs
@@ -667,7 +667,7 @@ impl Transform for CollecionArtTransform {
                     item.art = art;
                 }
                 // big screen uses thumbs for artwork.... while mobile uses the artwork. yeah...
-                item.thumb = item.art.clone();
+                // item.thumb = item.art.clone();
             }
         }
     }
