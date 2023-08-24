2369d00ab0bd906760d75b0104a53e4156b0389f
sarendsen <coding-mosses0z@icloud.com>
2023-08-24 14:19:31 +0200

    fix: force_maximum_quality

--- src/config.rs
+++ src/config.rs
@@ -71,6 +71,11 @@ pub struct Config {
         deserialize_with = "figment::util::bool_from_str_or_int"
     )]
     pub disable_transcode: bool,
+    #[serde(
+        default = "default_as_false",
+        deserialize_with = "figment::util::bool_from_str_or_int"
+    )]
+    pub force_maximum_quality: bool,
 }
 
 fn default_cache_ttl() -> u64 {
