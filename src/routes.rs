c57092440a7724400d50d7353c76b810fd5a78d8
lostb1t <coding-mosses0z@icloud.com>
2023-10-26 19:00:15 +0200

    fix: up timeout

--- src/routes.rs
+++ src/routes.rs
@@ -31,7 +31,7 @@ pub fn route() -> Router {
     let proxy = Proxy::with_client(
         config.host.clone().unwrap(),
         reqwest::Client::builder()
-            .timeout(Duration::from_secs(30))
+            .timeout(Duration::from_secs(60))
             .build()
             .unwrap(),
     );
@@ -39,7 +39,7 @@ pub fn route() -> Router {
     let mut router = Router::with_hoop(Cors::permissive().into_handler())
         .hoop(Logger::new())
         .hoop(should_skip)
-        .hoop(Timeout::new(Duration::from_secs(30)))
+        .hoop(Timeout::new(Duration::from_secs(60)))
         .hoop(Compression::new().enable_gzip(CompressionLevel::Fastest))
         //.hoop(max_concurrency(300))
         // .hoop(affix::inject(Arc::new(proxy.clone())));
